de5376a672097a4a0ca23dfe6f38420a7966a1b3
brandonjolley97 <bjol7457@740s-pl-131.local>
2015-10-07 09:59:38 -0600

    Added some constructors and imports
    Made the array list display in a popup

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -1,9 +1,30 @@
 package lists.controller;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+
 public class ArrayListController
 {
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
+	
 	public void start()
 	{
-		
+		firstWords.add("sdafsgkaoeartjoiasrsoijdf");
+		firstWords.add("DADDADADADADADADADBABABABABA");
+		showList();
+	}
+	
+	private void showList()
+	{
+		for(int spot = 0; spot  < firstWords.size(); spot++)
+		{
+			JOptionPane.showMessageDialog(null, firstWords.get(spot) + "is at spot# " + spot);
+		}
 	}
 }
